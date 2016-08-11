4831bc3298206c207d218bc3f21baa5500733391
juan rojas <carlos3434@hotmail.com>
2016-08-11 17:12:09 -0500

    data

--- data/ubicame.sql
+++ data/ubicame.sql
@@ -16,29 +16,170 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`ubicame` /*!40100 DEFAULT CHARACTER SET
 
 USE `ubicame`;
 
+/*Table structure for table `categorias` */
+
+DROP TABLE IF EXISTS `categorias`;
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(70) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `usuario_creacion` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `usuario_modificacion` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
 /*Data for the table `categorias` */
 
 insert  into `categorias`(`id`,`descripcion`,`estado`,`usuario_creacion`,`created_at`,`usuario_modificacion`,`updated_at`) values (1,'Otros',1,1,'2015-01-06 00:00:00',NULL,NULL);
 
+/*Table structure for table `detalles_usuarios` */
+
+DROP TABLE IF EXISTS `detalles_usuarios`;
+
+CREATE TABLE `detalles_usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) NOT NULL,
+  `campo` varchar(30) NOT NULL,
+  `value` varchar(100) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `usuario_creacion` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `usuario_modificacion` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `du_usuario_id` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*Data for the table `detalles_usuarios` */
 
+/*Table structure for table `logs_accesos` */
+
+DROP TABLE IF EXISTS `logs_accesos`;
+
+CREATE TABLE `logs_accesos` (
+  `id` int(11) NOT NULL,
+  `usuario_visita_id` int(11) NOT NULL,
+  `ubicacion_id` int(11) NOT NULL,
+  `fecha_ingreso` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*Data for the table `logs_accesos` */
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(70) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `usuario_creacion` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `usuario_modificacion` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 /*Data for the table `roles` */
 
 insert  into `roles`(`id`,`descripcion`,`estado`,`usuario_creacion`,`created_at`,`usuario_modificacion`,`updated_at`) values (1,'aéíóúÁÉÍÓÚÑñÁ..  ,, bueno',1,NULL,NULL,NULL,'2015-01-07 22:14:07'),(2,'rol2',0,NULL,NULL,NULL,NULL),(4,'probando nuevo',1,NULL,'2015-01-07 20:46:09',NULL,'2015-01-07 20:46:09'),(6,'ddtf',1,NULL,'2015-01-07 22:19:19',NULL,'2015-01-07 22:19:19'),(7,'new rol',1,NULL,'2015-01-08 03:31:58',NULL,'2015-01-08 03:31:58');
 
+/*Table structure for table `tipos_accesos` */
+
+DROP TABLE IF EXISTS `tipos_accesos`;
+
+CREATE TABLE `tipos_accesos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(80) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `usuario_creacion` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `usuario_modificacion` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
 /*Data for the table `tipos_accesos` */
 
 insert  into `tipos_accesos`(`id`,`descripcion`,`estado`,`usuario_creacion`,`created_at`,`usuario_modificacion`,`updated_at`) values (1,'Público',1,1,'2015-01-06 00:00:00',NULL,NULL),(2,'Privado',1,1,'2015-01-06 00:00:00',NULL,NULL);
+
+/*Table structure for table `tipos_usuarios` */
+
+DROP TABLE IF EXISTS `tipos_usuarios`;
+
+CREATE TABLE `tipos_usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(70) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `usuario_creacion` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `usuario_modificacion` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tipos_usuarios` */
 
 insert  into `tipos_usuarios`(`id`,`descripcion`,`estado`,`usuario_creacion`,`created_at`,`usuario_modificacion`,`updated_at`) values (1,'Regular',1,NULL,NULL,NULL,NULL),(2,'Empresa',1,NULL,NULL,NULL,NULL),(3,'Facebook',1,NULL,NULL,NULL,NULL),(4,'Google',1,NULL,NULL,NULL,NULL),(5,'Twiter',1,NULL,NULL,NULL,NULL);
 
+/*Table structure for table `ubicaciones` */
+
+DROP TABLE IF EXISTS `ubicaciones`;
+
+CREATE TABLE `ubicaciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` char(8) NOT NULL COMMENT 'Código único de la dirección 8 caracteres alfanumérico',
+  `usuario_id` int(11) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `tipo_acceso_id` int(11) NOT NULL,
+  `x` varchar(50) DEFAULT NULL,
+  `y` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `etiquetas` varchar(200) DEFAULT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `usuario_creacion` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `usuario_modificacion` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `codigo` (`codigo`),
+  KEY `usuario_ubicacion_id` (`usuario_id`),
+  KEY `tipo_acceso_id` (`tipo_acceso_id`),
+  KEY `categoria_id` (`categoria_id`),
+  CONSTRAINT `ub_categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `ub_tipo_acceso_id` FOREIGN KEY (`tipo_acceso_id`) REFERENCES `tipos_accesos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `ub_usuario_ubicacion_id` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
 /*Data for the table `ubicaciones` */
 
 insert  into `ubicaciones`(`id`,`codigo`,`usuario_id`,`categoria_id`,`tipo_acceso_id`,`x`,`y`,`descripcion`,`etiquetas`,`estado`,`usuario_creacion`,`created_at`,`usuario_modificacion`,`updated_at`) values (1,'a6b7k90g',1,1,1,'-12.111943','-77.011509','Open Plaza','centro comercial, open plaza, surquillo',1,1,'2015-01-06 00:00:00',NULL,NULL);
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_usuario_id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `remember_token` varchar(100) NOT NULL,
+  `url_validacion` varchar(100) DEFAULT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `usuario_creacion` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `usuario_modificacion` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tipo_usuario_id` (`tipo_usuario_id`),
+  CONSTRAINT `u_tipo_usuario_id` FOREIGN KEY (`tipo_usuario_id`) REFERENCES `tipos_usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 /*Data for the table `usuarios` */
 
